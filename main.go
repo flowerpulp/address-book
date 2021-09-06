c0db8cf2103ed03fb4cf7d7480ddca43dd9c269f
Konstantin <email@konstantinkoetzner.de>
2021-09-06 04:04:03 +0000

    Makes it read the file, unmarshal, marshal and print.

--- main.go
+++ main.go
@@ -2,8 +2,55 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"io/ioutil"
+	"encoding/json"
 )
 
+type entry struct {
+	Name         string `json:"Name"`
+	Email        string `json:"E-Mail,omitempty"`
+	Telefon      string `json:"Telefon,omitempty"`
+	Birthdate    string `json:"Geburtsdatum,omitempty"`
+}
+
+func sortJsonFile(fp string) error {
+	f, err := os.Open(fp)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(f)
+	fmt.Println(string(b))
+
+	e := &[]entry{}
+	fmt.Println(e)
+	err = json.Unmarshal(b, e)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(e)
+
+	bb, err := json.MarshalIndent(e, "", "        ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(bb))
+
+	return nil
+}
+
 func main() {
-	fmt.Println("Hi")
+	err := sortJsonFile("book.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
